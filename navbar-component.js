eb6e7f8433fc5a4c9175ffc93f20aaacc4d816cf
salmindev <salmin20shafii@gmail.com>
2025-03-05 13:50:20 +0300

    gtag

--- navbar-component.js
+++ navbar-component.js
@@ -215,30 +215,7 @@ class NavbarComponent extends LitElement {
      tawkScript.setAttribute("crossorigin", "*");
      document.head.appendChild(tawkScript);
      
-     const gtmNoscript = document.createElement('script');
-     gtmNoscript.innerHTML = `
-         <iframe src="https://www.googletagmanager.com/ns.html?id=AW-16835956925"
-                 height="0" width="0" style="display:none;visibility:hidden;"></iframe>
-     `; 
-     document.head.appendChild(gtmNoscript);
-         
-         const gtmScript = document.createElement('script');
-         gtmScript.textContent = `
-         (function(w, d, s, l, i) {
-             w[l] = w[l] || [];
-             w[l].push({
-                 'gtm.start': new Date().getTime(),
-                 event: 'gtm.js'
-             });
-             var f = d.getElementsByTagName(s)[0],
-                 j = d.createElement(s),
-                 dl = l != 'dataLayer' ? '&l=' + l : '';
-             j.async = true;
-             j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-             f.parentNode.insertBefore(j, f);
-         })(window, document, 'script', 'dataLayer', 'AW-16835956925'); // Replace 'GTM-WQTGVBG7' with your GTM ID
-     `;
-         document.head.appendChild(gtmScript);
+     
      
 
   
